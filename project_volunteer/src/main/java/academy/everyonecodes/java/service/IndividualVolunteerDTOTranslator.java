8037385918e62eb5c787b73f452dec32787fa938
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-08-09 15:07:18 +0200

    Created: IndividualVolunteerDTO and IndividualVolunteerDTOTranslator. Changes made: UserService (to translate DTO and save User)

---
+++ project_volunteer/src/main/java/academy/everyonecodes/java/service/IndividualVolunteerDTOTranslator.java
@@ -0,0 +1,2 @@
+package academy.everyonecodes.java.service;public class IndividualVolunteerDTOTranslator {
+}
--- project_volunteer/src/main/java/academy/everyonecodes/java/service/UserService.java
+++ project_volunteer/src/main/java/academy/everyonecodes/java/service/UserService.java
@@ -1,11 +1,11 @@
 package academy.everyonecodes.java.service;
 
+import academy.everyonecodes.java.data.IndividualVolunteerDTO;
 import academy.everyonecodes.java.data.Role;
 import academy.everyonecodes.java.data.User;
 import academy.everyonecodes.java.data.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import java.util.Set;
@@ -17,16 +17,22 @@ public class UserService {
     private final UserRepository userRepository;
     private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
+    private final IndividualVolunteerDTOTranslator individualVolunteerDTOTranslator;
 
     public UserService(UserRepository userRepository,
                        RoleService roleService,
-                       PasswordEncoder passwordEncoder) {
+                       PasswordEncoder passwordEncoder, IndividualVolunteerDTOTranslator individualVolunteerDTOTranslator) {
         this.roleService = roleService;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.individualVolunteerDTOTranslator = individualVolunteerDTOTranslator;
     }
 
-    public User save(@Valid User user) {
+    public User translateDtoAndSaveUser(IndividualVolunteerDTO individualVolunteerDTO) {
+        return save(individualVolunteerDTOTranslator.translateDTOtoUser(individualVolunteerDTO));
+    }
+
+    public User save(User user) {
         user.setPassword(encryptPasswordFromUser(user));
         if (user.getRoles() == null)
             return null;
