abedf594f8a358653ecb9f2cd7e8ba94d847b995
Ramy Faris <ramy1@gmx.at>
2021-08-12 10:04:16 +0200

    Implemented ProfileDTO with abstract class. Refactored ProfileDTOEndpoint & Implemented new viewProfile Method. Deleted Tests as they need to be writen completely new. Modified UserService.throwBadRequest to be able to input errorMessage from yml file

---
+++ project_volunteer/src/main/java/academy/everyonecodes/java/data/ProfileDTOs/ProfileDTO.java
@@ -0,0 +1,148 @@
+package academy.everyonecodes.java.data.ProfileDTOs;
+
+import academy.everyonecodes.java.data.Role;
+
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class ProfileDTOs
+{
+    private String username;
+    private String postalCode;
+    private String city;
+    private String street;
+    private String streetNumber;
+    private String email;
+    private String telephoneNumber;
+    private String description;
+    private Set<Role> roles;
+
+    public ProfileDTOs()
+    {
+    }
+
+    public ProfileDTOs(String username, String email, Set<Role> roles)
+    {
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public ProfileDTOs(String username, String postalCode, String city, String street, String streetNumber, String email, String telephoneNumber, String description, Set<Role> roles)
+    {
+        this.username = username;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.description = description;
+        this.roles = roles;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPostalCode()
+    {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode)
+    {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getStreet()
+    {
+        return street;
+    }
+
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
+
+    public String getStreetNumber()
+    {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber)
+    {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getTelephoneNumber()
+    {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber)
+    {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Set<Role> getRoles()
+    {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles)
+    {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProfileDTOs that = (ProfileDTOs) o;
+        return Objects.equals(username, that.username) && Objects.equals(postalCode, that.postalCode) && Objects.equals(city, that.city) && Objects.equals(street, that.street) && Objects.equals(streetNumber, that.streetNumber) && Objects.equals(email, that.email) && Objects.equals(telephoneNumber, that.telephoneNumber) && Objects.equals(description, that.description) && Objects.equals(roles, that.roles);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(username, postalCode, city, street, streetNumber, email, telephoneNumber, description, roles);
+    }
+}
