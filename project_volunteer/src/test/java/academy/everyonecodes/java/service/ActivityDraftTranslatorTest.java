01bb90dd16dd3ede9a57fafaccba9e51c13bbc57
Ramy Faris <ramy1@gmx.at>
2021-08-18 15:45:27 +0200

    Finalized ActivityDraftTranslatorTest.

--- project_volunteer/src/test/java/academy/everyonecodes/java/service/ActivityDraftTranslatorTest.java
+++ project_volunteer/src/test/java/academy/everyonecodes/java/service/ActivityDraftTranslatorTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.ValidationException;
 import javax.validation.Validator;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,7 +20,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ActivityDraftTranslatorTest
@@ -32,71 +31,68 @@ public class ActivityDraftTranslatorTest
     @Autowired
     Validator validator;
 
-    Set<Role> roles = new HashSet<>(List.of(new Role(2L, "ROLE_INDIVIDUAL")));
-    User organizer = new User(
-            "username",
-            "password",
-            "email@email.com",
-            roles
-    );
-    Set<User> applicants = new HashSet<>();
-    Set<User> participants = new HashSet<>();
     String categoriesString = "oneCategory";
 
     List<String> categories = List.of(categoriesString);
 
     @Test
-    void toActivity_valid_all_fields() {
+    void toActivity_valid_all_fields()
+    {
         Draft draft = new Draft(
                 "title",
-                "descr",
-                "skills",
+                "description",
+                "recommendedSkills",
                 categoriesString,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                false,
-                organizer.getUsername());
+                true,
+                null);
 
         Activity expected = new Activity(
                 "title",
-                "descr",
-                "skills",
+                "description",
+                "recommendedSkills",
                 categories,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                false,
+                true,
                 null,
-                applicants,
-                participants);
+                new HashSet<>(),
+                new HashSet<>()
+        );
 
-        Activity actual = translator.toActivity(draft);
-        Assertions.assertEquals(expected, actual);
+        var actual = translator.toActivity(draft);
+        assertEquals(expected, actual);
+        assertNoViolations(draft);
     }
 
     @Test
-    void toActivity_valid_only_mandatory_fields() {
+    void toActivity_valid_only_mandatory_fields()
+    {
         Draft draft = new Draft(
                 "title",
-                "descr",
+                "description",
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                false,
-                organizer.getUsername());
+                true,
+                null);
 
         Activity expected = new Activity(
                 "title",
-                "descr",
+                "description",
                 null,
                 new ArrayList<>(),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                false,
+                true,
                 null,
-                applicants,
-                participants);
+                new HashSet<>(),
+                new HashSet<>()
+        );
 
-        Activity actual = translator.toActivity(draft);
-        Assertions.assertEquals(expected, actual);
+        var actual = translator.toActivity(draft);
+        assertEquals(expected, actual);
+        assertNoViolations(draft);
     }
 
     @Test
@@ -105,162 +101,101 @@ public class ActivityDraftTranslatorTest
     {
         Draft draft = new Draft(
                 "",
-                "descr",
+                "description",
+                "recommendedSkills",
+                categoriesString,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                false,
-                organizer.getUsername());
+                true,
+                null);
 
-
-
-        Set<ConstraintViolation<Activity>> constraintViolations = validator.validate(translator.toActivity(draft));
-        System.out.println(constraintViolations);
-        System.out.println("\n" + constraintViolations.size());
-        assertTrue(!constraintViolations.isEmpty());
-//        draft.setTitle("asgdhfjkasdgfktrqeotieüptipotjerqogjnsdfkjnkdfngbkjldshgtjkhgkjrehgrejkqhgrqeiotghewiutgerwiouzgiwerzvuioszviuoefziouqwezriouzreto");
-
-//        assertThrows(ValidationException.class, () -> {
-//            translator.toActivity(draft);
-//        });
+        assertViolations(draft);
+        draft.setTitle("asgdhfjkasdgfktrqeotieüptipotjerqogjnsdfkjnkdfngbkjldshgtjkhgkjrehgrejkqhgrqeiotghewiutgerwiouzgiwerzvuioszviuoefziouqwezriouzreto");
+        assertViolations(draft);
     }
 
     @Test
     @WithMockUser(username = "test", password = "test", authorities = {"ROLE_INDIVIDUAL"})
     void postActivity_DESCRIPTION_empty() throws Exception
     {
-        Activity activity = new Activity(
+        Draft draft = new Draft(
                 "title",
                 "",
                 "recommendedSkills",
-                categories,
+                categoriesString,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 true,
-                organizer,
-                applicants,
-                participants
-        );
+                null);
 
+        assertViolations(draft);
     }
 
     @Test
     @WithMockUser(username = "test", password = "test", authorities = {"ROLE_INDIVIDUAL"})
     void postActivity_START_DATE_TIME_empty__not_in_future() throws Exception
     {
-        Activity activity = new Activity(
+        Draft draft = new Draft(
                 "title",
                 "description",
                 "recommendedSkills",
-                categories,
+                categoriesString,
                 null,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 true,
-                organizer,
-                applicants,
-                participants
-        );
+                null);
 
-        activity.setStartDateTime(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(10, 10, 10)));
+        assertViolations(draft);
+        draft.setStartDateTime(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(10, 10, 10)));
+        assertViolations(draft);
     }
 
     @Test
     @WithMockUser(username = "test", password = "test", authorities = {"ROLE_INDIVIDUAL"})
     void postActivity_END_DATE_TIME_empty__not_in_future() throws Exception
     {
-        Activity activity = new Activity(
+        Draft draft = new Draft(
                 "title",
                 "description",
                 "recommendedSkills",
-                categories,
+                categoriesString,
                 LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 null,
                 true,
-                organizer,
-                applicants,
-                participants
-        );
+                null);
 
-        activity.setEndDateTime(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(10, 10, 10)));
+        assertViolations(draft);
+        draft.setEndDateTime(LocalDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(10, 10, 10)));
+        assertViolations(draft);
     }
 
     @Test
     @WithMockUser(username = "test", password = "test", authorities = {"ROLE_INDIVIDUAL"})
     void postActivity_IS_OPEN_END_empty__false() throws Exception
     {
-
-        Activity activity = new Activity(
-                "title",
-                "description",
-                "recommendedSkills",
-                categories,
-                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                null,
-                organizer,
-                applicants,
-                participants
-        );
-
-        activity.setOpenEnd(false);
-    }
-
-    @Test
-    @WithMockUser(username = "test", password = "test", authorities = {"ROLE_INDIVIDUAL"})
-    void postActivity_ORGANIZER_is_null() throws Exception
-    {
-        Activity activity = new Activity(
-                "title",
-                "description",
-                "recommendedSkills",
-                categories,
-                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
-                true,
-                null,
-                applicants,
-                participants
-        );
-
-    }
-
-    @Test
-    void toActivity_all_left_out_fields() {
         Draft draft = new Draft(
+                "title",
+                "description",
+                "recommendedSkills",
+                categoriesString,
+                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
+                LocalDateTime.of(LocalDate.of(2100, 1, 1), LocalTime.of(10, 10, 10)),
                 null,
-                null,
-                null,
-                "",
-                null,
-                null,
-                true,
                 null);
-
-        Activity expected = new Activity(
-                null,
-                null,
-                null,
-                List.of(""),
-                null,
-                null,
-                true,
-                null,
-                applicants,
-                participants);
-
-        Activity actual = translator.toActivity(draft);
-        Assertions.assertEquals(expected, actual);
+        assertViolations(draft);
+        draft.setOpenEnd(false);
+        assertNoViolations(draft);
     }
 
     private void assertNoViolations(Draft draft)
     {
         Set<ConstraintViolation<Activity>> constraintViolations = validator.validate(translator.toActivity(draft));
-        System.out.println(constraintViolations);
-        System.out.println("\n" + constraintViolations.size());
-        assertTrue(!constraintViolations.isEmpty());
+        assertTrue(constraintViolations.isEmpty());
     }
 
     private void assertViolations(Draft draft)
     {
-
+        Set<ConstraintViolation<Activity>> constraintViolations = validator.validate(translator.toActivity(draft));
+        assertFalse(constraintViolations.isEmpty());
     }
 }
