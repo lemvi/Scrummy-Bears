eec921f9bc35e60a17a8459a0b8daba37c6cd3da
Weidinger-Georg-Work <77104921+Weidinger-Georg-Work@users.noreply.github.com>
2021-08-23 13:39:13 +0200

    F10 web app tree (#8)
    * v 0.01 - Began implementing a basic solution
    
    * v 0.91 - Solution implemented, Tests completed, yml File filled mostly
    
    * v 1.01 - Added further Endpoints
    
    * v 1.02 - deleted unnecessary code comments, Corrected some VOLUNTEERs to ROLE_VOLUNTEER
    
    * v 1.03 - corrected incorrect ROLE_INDIVIDUAL to ROLE_VOLUNTEER

---
+++ project_volunteer/src/main/java/academy/everyonecodes/java/service/WebAppTreeService.java
@@ -0,0 +1,135 @@
+package academy.everyonecodes.java.service;
+
+import academy.everyonecodes.java.data.ResourceInfo;
+import academy.everyonecodes.java.data.Role;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Service
+@ConfigurationProperties("webapptreelist")
+public class WebAppTreeService {
+
+	public String prepareWebAppTree() {
+		return prepareOutput();
+	}
+
+	/*private final List<ResourceInfo> resourceInfos = List.of(
+			new ResourceInfo("Account", "Edit Account", "Make changes in Profile", "/account/{username}", "PUT", Set.of(new Role("ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+			new ResourceInfo("Account", "View Account", "View Account Info", "/account/{username}", "GET", Set.of(new Role("ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+			new ResourceInfo("Profile", "View Profile", "View Profile as seen by others", "/profile/{username}", "GET", Set.of(new Role("ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+			new ResourceInfo("Profile", "View Profile", "View Profile as seen by others", "/profile/{username}", "GET", Set.of(new Role("ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+			new ResourceInfo("Profile", "View Volunteer", "View Profile as seen by others", "/profile/{username}", "GET", Set.of(new Role("ROLE_VOLUNTEER"))),
+			new ResourceInfo("Profile", "View Individual", "View Profile as seen by others", "/profile/{username}", "GET", Set.of(new Role("ROLE_INDIVIDUAL"))),
+			new ResourceInfo("Profile", "View Company", "View Profile as seen by others", "/profile/{username}", "GET", Set.of(new Role("ROLE_COMPANY")))
+	);*/
+
+	private List<ResourceInfo> resourceinfos;
+	/*= List.of(
+
+			new ResourceInfo("Account", "View my Account", "View my Account Info", "/account/{username}", "GET", Set.of(new Role(
+					"ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Account", "Edit my Account", "Make changes in my Account", "/account/{username}", "PUT", Set.of(new Role(
+					"ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Account", "Add Skills", "Add skills to my Account", "/addSkill/{username}", "POST",
+					Set.of(new Role("ROLE_VOLUNTEER"))),
+
+			new ResourceInfo("Profile", "View Profile as seen by others", "View my Profile as seen by Others", "/profile/{username}", "GET"
+					, Set.of(new Role("ROLE_VOLUNTEER"), new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Search", "Search for activities as Volunteer", "Search for Activities as Volunteer", "/search/{text}", "GET",
+					Set.of(new Role("ROLE_VOLUNTEER"))),
+
+
+
+			new ResourceInfo("Activities", "Create Activity", "Create a new Activity as Organizer", "/activities", "POST",
+					Set.of(new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "View all Activities", "View all my Activities as Organizer", "/activities", "GET",
+					Set.of(new Role("ROLE_INDIVIDUAL"), new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "Create Draft", "Create a new Draft as Organizer", "/drafts", "POST", Set.of(new Role("ROLE_INDIVIDUAL"),
+					new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "View all Drafts", "View all my Drafts as Organizer", "/drafts", "GET", Set.of(new Role("ROLE_INDIVIDUAL")
+					, new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "Edit Draft", "Edit created Draft as Organizer", "/drafts", "PUT", Set.of(new Role("ROLE_INDIVIDUAL"),
+					new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "Save Draft as Activity", "Save my Draft as Activity as Organizer", "/drafts/{draftId}", "PUT",
+					Set.of(new Role(
+							"ROLE_INDIVIDUAL"),    new Role("ROLE_COMPANY"))),
+
+			new ResourceInfo("Activities", "View all my Activities", "View all my Activities as Volunteer", "/{username}/activities", "GET",
+					Set.of(new Role("ROLE_VOLUNTEER"))),
+
+			new ResourceInfo("Activities", "View all my completed Activities", "View all my completed Activities as Volunteer",
+					"/{username" +
+							"}/activities/completed", "GET",
+					Set.of(new Role("ROLE_VOLUNTEER"))),
+
+			new ResourceInfo("Activities", "View all my pending Activities", "View all my pending Activities as Volunteer",
+					"/{username" +
+							"}/activities/pending", "GET",
+					Set.of(new Role("ROLE_VOLUNTEER"))),
+
+			new ResourceInfo("Activities", "View all my active Activities", "View all my active Activities as Volunteer",
+					"/{username" +
+							"}/activities/active", "GET",
+					Set.of(new Role("ROLE_VOLUNTEER")))
+	);*/
+
+	public void setResourceinfos(List<ResourceInfo> resourceinfos) {
+		this.resourceinfos = resourceinfos;
+	}
+
+	private String prepareOutput() {
+		var output = new StringBuilder();
+		Collection<? extends GrantedAuthority> authorities = getAuthorities();
+
+		Map<String, List<ResourceInfo>> resourceMap = resourceinfos.stream()
+				.filter(element -> checkIfAuthoritiesAreInAllowedRoles(element.getAllowedRoles(), authorities))
+				.collect(groupingBy(ResourceInfo::getCategory));
+
+		for (Map.Entry<String, List<ResourceInfo>> mapEntry : resourceMap.entrySet()) {
+			output
+					.append(mapEntry.getKey())
+					.append("\n");
+			for (ResourceInfo resourceInfo : mapEntry.getValue()) {
+				output
+						.append("    ")
+						.append(resourceInfo.getName())
+						.append("\n");
+			}
+		}
+		return output.toString();
+	}
+
+	private Authentication getAuthentication() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+
+	private Collection<? extends GrantedAuthority> getAuthorities() {
+		return getAuthentication().getAuthorities();
+	}
+
+	private boolean checkIfAuthoritiesAreInAllowedRoles(Set<Role> allowedRoles, Collection<? extends GrantedAuthority> authorities) {
+		List<String> authoritiesStringList = authorities.stream()
+				.map(GrantedAuthority::getAuthority)
+				.collect(Collectors.toList());
+		List<String> allowedRolesStringList = allowedRoles.stream()
+				.map(Role::getRole)
+				.collect(Collectors.toList());
+		return authoritiesStringList.stream().anyMatch(allowedRolesStringList::contains);
+	}
+}
