9ab41737e2a17f6cd270e6e1e24a70d69654bba1
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-08-23 13:08:08 +0200

    modified: RoleRepository (removed unused method) RoleService (added max_connection value for DB) removed dependency that was not needed

--- project_volunteer/src/main/java/academy/everyonecodes/java/data/RoleRepository.java
+++ project_volunteer/src/main/java/academy/everyonecodes/java/data/RoleRepository.java
@@ -2,6 +2,11 @@ package academy.everyonecodes.java.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRole(String role);
+    Optional<Role> findByRole(String role);
+
+    List<Role> findAll();
 }
--- project_volunteer/src/main/java/academy/everyonecodes/java/service/RoleService.java
+++ project_volunteer/src/main/java/academy/everyonecodes/java/service/RoleService.java
@@ -4,6 +4,9 @@ import academy.everyonecodes.java.data.Role;
 import academy.everyonecodes.java.data.RoleRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -16,10 +19,17 @@ public class RoleService {
         this.roles = roles;
     }
 
-    public void saveAllRolesInDB() {
+    public void saveAllRolesInDB() throws SQLException {
+        List<Role> allRolesFromDb = roleRepository.findAll();
         for (Role role : roles)
         {
-            roleRepository.save(role);
+            if (!allRolesFromDb.contains(role))
+                roleRepository.save(role);
         }
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/project_volunteer?useSSL=false", "root", "root");
+        String sql = "SET GLOBAL max_connections=2048";
+        Statement statement = con.createStatement();
+        statement.execute(sql);
+        con.close();
     }
 }
