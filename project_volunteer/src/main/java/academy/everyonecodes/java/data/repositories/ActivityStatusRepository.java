ea9163362e115cea42f04299a08d06a588cdfab1
Pundito7 <pundito7@gmail.com>
2021-08-25 10:40:21 +0200

    refactored StatusHandler & Tests incl. APPLIED, COMPLETED new

---
+++ project_volunteer/src/main/java/academy/everyonecodes/java/data/repositories/ActivityStatusRepository.java
@@ -0,0 +1,13 @@
+package academy.everyonecodes.java.data.repositories;
+
+import academy.everyonecodes.java.data.Activity;
+import academy.everyonecodes.java.data.ActivityStatus;
+import academy.everyonecodes.java.data.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ActivityStatusRepository extends JpaRepository<ActivityStatus, Long> {
+
+    Optional<Status> findByActivity(Activity activity);
+}
--- project_volunteer/src/test/java/academy/everyonecodes/java/service/StatusHandlerTest.java
+++ project_volunteer/src/test/java/academy/everyonecodes/java/service/StatusHandlerTest.java
@@ -1,8 +1,10 @@
 package academy.everyonecodes.java.service;
 
 import academy.everyonecodes.java.data.Activity;
+import academy.everyonecodes.java.data.ActivityStatus;
 import academy.everyonecodes.java.data.Status;
 import academy.everyonecodes.java.data.User;
+import academy.everyonecodes.java.data.repositories.ActivityStatusRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,78 +29,66 @@ class StatusHandlerTest {
     @MockBean
     private UserService userService;
 
-    @Value("${errorMessages.usernameNotFound}")
-    private String userNotFoundErrorMessage;
+    @MockBean
+    private ActivityStatusRepository activityStatusRepository;
 
     @Test
-    void getStatusForSpecificActivityAndVolunteer_PendingWhenActivityEndDateInFutureAndApplicant() {
-        User user = new User();
-        Long userId = 1L;
-        user.setId(userId);
-        Activity activity = new Activity();
-        activity.setEndDateTime(LocalDateTime.MAX);
-        activity.setApplicants(Set.of(user));
-
-        when(userService.findById(userId)).thenReturn(Optional.of(user));
-
-        Status expected = Status.PENDING;
-        Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, user.getId());
-
-        assertEquals(expected, actual);
-
-        verify(userService).findById(userId);
-        verifyNoMoreInteractions(userService);
-    }
-
-    @Test
-    void getStatusForSpecificActivityAndVolunteer_PendingWhenActivityHasOpenEndAndApplicant() {
-        User user = new User();
-        Long userId = 1L;
-        user.setId(userId);
+    void getStatusForSpecificActivity_CompletedWhenOrganizerSetsStatusCompleted() {
         Activity activity = new Activity();
         activity.setEndDateTime(LocalDateTime.MIN);
-        activity.setOpenEnd(true);
-        activity.setApplicants(Set.of(user));
-
-        when(userService.findById(userId)).thenReturn(Optional.of(user));
-
-        Status expected = Status.PENDING;
-        Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, user.getId());
-
-        assertEquals(expected, actual);
-
-        verify(userService).findById(userId);
-        verifyNoMoreInteractions(userService);
-    }
-
-    @Test
-    void getStatusForSpecificActivityAndVolunteer_CompletedWhenEndTimeInPastAndNotOpenEnd() {
-        User user = new User();
-        Long userId = 1L;
-        user.setId(userId);
-        Activity activity = new Activity();
-        activity.setEndDateTime(LocalDateTime.MIN);
-        activity.setApplicants(Set.of(user));
+        activity.setApplicants(Set.of());
+        activity.setParticipants(Set.of());
         activity.setOpenEnd(false);
 
-        when(userService.findById(userId)).thenReturn(Optional.of(user));
+        when(userService.findById(1L)).thenReturn(Optional.of(new User()));
+        when(activityStatusRepository.findByActivity(activity)).thenReturn(Optional.of(Status.COMPLETED));
 
         Status expected = Status.COMPLETED;
+        Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, 1L);
+
+        assertEquals(expected, actual);
+
+
+        verify(activityStatusRepository).findByActivity(activity);
+        verify(userService).findById(1L);
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    void getStatusForSpecificActivityAndVolunteer_ActiveWhenStartDateInPastAndActivityHasOpenEndAndParticipant() {
+        User user = new User();
+        Long userId = 1L;
+        user.setId(userId);
+        Activity activity = new Activity();
+        activity.setStartDateTime(LocalDateTime.MIN);
+        activity.setEndDateTime(LocalDateTime.MIN);
+        activity.setOpenEnd(true);
+        activity.setApplicants(Set.of());
+        activity.setParticipants(Set.of(user));
+
+        when(userService.findById(userId)).thenReturn(Optional.of(user));
+        when(activityStatusRepository.findByActivity(activity)).thenReturn(Optional.of(Status.NOT_SET));
+
+        Status expected = Status.ACTIVE;
         Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, user.getId());
 
         assertEquals(expected, actual);
 
         verify(userService).findById(userId);
+        verify(activityStatusRepository).findByActivity(activity);
         verifyNoMoreInteractions(userService);
     }
 
+
     @Test
-    void getStatusForSpecificActivityAndVolunteer_ActiveWhenEndDateInFutureAndParticipant() {
+    void getStatusForSpecificActivityAndVolunteer_ActiveWhenStartDateInPastAndEndDateInFutureAndParticipant() {
         User user = new User();
         Long userId = 1L;
         user.setId(userId);
         Activity activity = new Activity();
+        activity.setStartDateTime(LocalDateTime.now().minusMinutes(1L));
         activity.setEndDateTime(LocalDateTime.MAX);
+        activity.setOpenEnd(false);
         activity.setApplicants(Set.of());
         activity.setParticipants(Set.of(user));
 
@@ -112,4 +102,52 @@ class StatusHandlerTest {
         verify(userService).findById(userId);
         verifyNoMoreInteractions(userService);
     }
+
+    @Test
+    void getStatusForSpecificActivityAndVolunteer_AppliedWhenActivityStartDateInFutureAndApplicant() {
+        User user = new User();
+        Long userId = 1L;
+        user.setId(userId);
+        Activity activity = new Activity();
+        activity.setStartDateTime(LocalDateTime.now().plusMinutes(1L));
+        activity.setEndDateTime(LocalDateTime.MAX);
+        activity.setOpenEnd(false);
+        activity.setApplicants(Set.of(user));
+        activity.setParticipants(Set.of());
+
+        when(userService.findById(userId)).thenReturn(Optional.of(user));
+
+        Status expected = Status.APPLIED;
+        Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, user.getId());
+
+        assertEquals(expected, actual);
+
+        verify(userService).findById(userId);
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    void getStatusForSpecificActivityAndVolunteer_PendingWhenActivityStartDateInFutureAndParticipant() {
+        User user = new User();
+        Long userId = 1L;
+        user.setId(userId);
+        Activity activity = new Activity();
+        activity.setStartDateTime(LocalDateTime.now().plusMinutes(1L));
+        activity.setEndDateTime(LocalDateTime.MAX);
+        activity.setOpenEnd(false);
+        activity.setApplicants(Set.of());
+        activity.setParticipants(Set.of(user));
+
+        when(userService.findById(userId)).thenReturn(Optional.of(user));
+
+        Status expected = Status.PENDING;
+        Status actual = statusHandler.getStatusForSpecificActivityAndVolunteer(activity, user.getId());
+
+        assertEquals(expected, actual);
+
+        verify(userService).findById(userId);
+        verifyNoMoreInteractions(userService);
+    }
+
+
 }
